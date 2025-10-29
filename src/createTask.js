1229c333f8159d80c64ccec963f5d797907d63f1
Aritra Saha <158503009+devritra@users.noreply.github.com>
2025-10-29 22:41:48 +0530

    Created task creator
    created the module that creates the task objects with the provided task details
    
    also created a test js file to provide some dummy details to the task creator

---
+++ src/createTask.js
@@ -0,0 +1,38 @@
+import { title, description, dueDate, priority } from "./test";
+class createTask{
+    #title;
+    #description;
+    #dueDate;
+    #priority;
+    constructor(title, description, dueDate, priority){
+        this.#title = title;
+        this.#description = description;
+        this.#dueDate = dueDate;
+        this.#priority = priority;
+    }
+    getTitle(){
+        return this.#title;
+    }
+    setTitle(newTitle){
+        this.#title = newTitle;
+    }
+    getDescription(){
+        return this.#description;
+    }
+    setDescription(newDescription){
+        this.#description = newDescription;
+    }
+    getDueDate(){
+        return this.#dueDate;
+    }
+    setDueDate(newDueDate){
+        this.#dueDate = newDueDate;
+    }
+    getPriority(){
+        return this.#priority;
+    }
+    setPriority(newPriority){
+        this.#priority = newPriority;
+    }
+}
+export const todo = new createTask(title, description, dueDate, priority);
---
+++ src/test.js
@@ -0,0 +1,4 @@
+export const title = "task baby";
+export const description = "too ez";
+export const dueDate = "lolol";
+export const priority = "888";
