d5e606c8d57acc1006a7752643e30c3e1057e820
Aritra Saha <158503009+devritra@users.noreply.github.com>
2025-11-07 20:45:35 +0530

    Tasks are being added to the task array
    they are also showing up on the page

--- src/eventHandler.js
+++ src/eventHandler.js
@@ -1,12 +1,8 @@
 import { loadTaskCOntentToDom } from "./taskContentLoader";
 // import { removeTaskFromTaskArray } from "./removeTaskFromTaskArray";
-// import { createTask } from "./createTask";
-// import { operateOnTaskArray } from "./operateOnTaskArray";
+import { createTask } from "./createTask";
+import { operateOnTaskArray } from "./operateOnTaskArray";
 
-// let title;
-// let description;
-// let dueDate;
-// let priority;
 const main = document.querySelector(".main");
 main.addEventListener("click", (e)=>{
     switch (e.target.dataset.action) {
@@ -30,24 +26,29 @@ main.addEventListener("click", (e)=>{
             const taskAddingDialog = document.querySelector("#task_adding_dialog");
             taskAddingDialog.showModal();
             break;
-//         case "add_task_from_dialog":
-//             const taskTitleInput = document.querySelector("#task_title_input");
-//             const taskDescInput = document.querySelector("#task_desc_input");
-//             const taskDueDateInput = document.querySelector("#task_date_input");
-//             const taskPriorityInputList = document.getElementsByName("priority");
-//             for(let i=0; i<taskPriorityInputList.length; i++){
-//                 if(taskPriorityInputList[i].checked){
-//                     priority = taskPriorityInputList[i].value;
-//                 }
-//             }
-//             title = taskTitleInput.value;
-//             description = taskDescInput.value;
-//             dueDate = taskDueDateInput.value;
-//             const newTask = new createTask(title, description, dueDate, priority);
-//             operateOnTaskArray(newTask);
-//             const taskAddingDialogAgain = document.querySelector("#task_adding_dialog");
-//             taskAddingDialogAgain.close();
-//             loadTaskCOntentToDom();
+        case "add_task_from_dialog":
+            const taskTitleInput = document.querySelector("#task_title_input");
+            const taskDescInput = document.querySelector("#task_desc_input");
+            const taskDueDateInput = document.querySelector("#task_date_input");
+            const taskPriorityInputList = document.getElementsByName("priority");
+            let title;
+            let description;
+            let dueDate;
+            let priority;
+            for(let i=0; i<taskPriorityInputList.length; i++){
+                if(taskPriorityInputList[i].checked){
+                    priority = taskPriorityInputList[i].value;
+                }
+            }
+            title = taskTitleInput.value;
+            description = taskDescInput.value;
+            dueDate = taskDueDateInput.value;
+            const newTask = new createTask(title, description, dueDate, priority);
+            operateOnTaskArray(newTask);
+            const taskAddingDialogAgain = document.querySelector("#task_adding_dialog");
+            taskAddingDialogAgain.close();
+            loadTaskCOntentToDom();
+            break;
     }
 })
 // export {title, description, dueDate, priority};
